687b24765f0e30691f7ab9c0243ca4331acdf996
Anmol Anand <76034132+anmol420@users.noreply.github.com>
2023-12-26 03:20:05 +0530

    Added NewItem Page Widget

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 
 import 'package:shopping_list_app/data/dummy_items.dart';
+import 'package:shopping_list_app/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void _addItem() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItem(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +26,12 @@ class GroceryList extends StatelessWidget {
         title: const Text(
           'Your Groceries',
         ),
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: const Icon(Icons.add),
+          ),
+        ],
       ),
       body: ListView.builder(
         itemCount: groceryItems.length,
